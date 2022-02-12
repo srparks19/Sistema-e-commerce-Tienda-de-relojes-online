1e700f16dc28a0854b6e8d5ea3ca29f8724bbb6b
Sebastian Pérez López <shanlonp4@gmail.com>
2022-02-12 18:03:10 -0500

    login de usuario

--- producto.php
+++ producto.php
@@ -1,92 +1,109 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perpet</title>
-
-    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Mi sistema E-Commerce</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index-eje.css">
 </head>
 <body>
-    <header>
-        <div class="logo-place"><img src="assets/logo.png"></div>
-        <div class="search-place">
-            <input class="search-place" type="text" id="idbusqueda" placeholder="Busca el reloj de tus sueños">
-            <button class="btn-main btn-search"><i class="fa fa-search" aria-hidden="true"></i></button>
-        </div>
-        <div class="options-place">
-            <div class="item-option" title="Registrarse"><i class="fa fa-user-circle-o" aria-hidden="true"></i></div>
-            <div class="item-option" title="Iniciar sesion"><i class="fa fa-sign-in" aria-hidden="true"></i></div>
-            <div class="item-option" title="Carrito de compras"><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>
-        </div>
-    </header>
-    <div class="main-content">
-        <div class="content-page">
-            <section>
-                <div class="part1">
-                    <img id="idimg" src="assets/reloj2.jpg">
-                </div>
-                <div class="part2">
-                    <h2 id="idtitle">NOMBRE PRINCIPAL</h2>
-                    <h1 id="idprice">S/. 35. <span>99</span></h1>
-                    <h3 id="iddescription">Descripcion del producto</h3>
-                    <button>Comprar</button>
-                </div>
-            </section>
-            <div class="title-section">Productos destacados</div>
-            <div class="product-list" id="space-list">
-            </div>
-        </div>
-    </div>
-    <script type="text/javascript">
-        var p='<?php echo $_GET["p"]; ?>';
-    </script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $.ajax({
-                url:'servicios/producto/get_all_products.php',
-                type: 'POST',
-                data:{},
-                success: function(data){
-                    let html='';
-                    console.log(data)
-                    for(var i = 0; i < data.datos.length; i++){
-                        if(data.datos[i].codpro==p){
-                            document.getElementById("idimg").src=data.datos[i].rutimapro;
-                            document.getElementById("idtitle").innerHTML=data.datos[i].nompro;
-                            document.getElementById("idprice").innerHTML=data.datos[i].prepro;
-                            document.getElementById("iddescription").innerHTML=data.datos[i].despro;
-                        }
-                        html+=
-                        '<div class="product-box">'+
-                            '<a href="producto.php?p=' + data.datos[i].codpro + '">'+
-                                '<div class="product">'+
-                                    '<img src="' + data.datos[i].rutimapro +'">'+
-                                    '<div class="detail-title">' + data.datos[i].nompro +'</div>'+
-                                    '<div class="detail-description">' + data.datos[i].despro +'</div>'+
-                                    '<div class="detail-price">' + formato_precio(data.datos[i].prepro) +'</div>'+
-                                '</div>'+
-                            '</a>'+
-                        '</div>';
-                    }
-                    document.getElementById("space-list").innerHTML=html;
-                },
-                error:function(err){
-                    console.error(err)
-                }
-            });
-        });
-        function formato_precio(valor){
-            let $valor = valor.toString();
-            let array = $valor.split(".");
-            return "S/. " + array[0] + ".<span>" + array[1] + "</span>";
-        }
-    </script>
+	<?php include("layouts/_main-header.php"); ?>
+	<div class="main-content">
+		<div class="content-page">
+			<section>
+				<div class="part1">
+					<img id="idimg" src="assets/products/crepe.jpg">
+				</div>
+				<div class="part2">
+					<h2 id="idtitle">NOMBRE PRINCIPAL</h2>
+					<h1 id="idprice">S/. 35.<span>99</span></h1>
+					<h3 id="iddescription">Descripcion del producto</h3>
+					<button onclick="iniciar_compra()">Comprar</button>
+				</div>
+			</section>
+			<div class="title-section">Productos destacados</div>
+			<div class="products-list" id="space-list">
+			</div>
+		</div>
+	</div>
+	<?php include("layouts/_footer.php"); ?>
+	<script type="text/javascript" src="js/main-scripts.js"></script>
+	<script type="text/javascript">
+		var p='<?php echo $_GET["p"]; ?>';
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$.ajax({
+				url:'servicios/producto/get_all_products.php',
+				type:'POST',
+				data:{},
+				success:function(data){
+					console.log(data);
+					let html='';
+					for (var i = 0; i < data.datos.length; i++) {
+						if (data.datos[i].codpro==p) {
+							document.getElementById("idimg").src=data.datos[i].rutimapro;
+							document.getElementById("idtitle").innerHTML=data.datos[i].nompro;
+							document.getElementById("idprice").innerHTML=formato_precio(data.datos[i].prepro);
+							document.getElementById("iddescription").innerHTML=data.datos[i].despro;
+						}
+						html+=
+						'<div class="product-box">'+
+							'<a href="producto.php?p='+data.datos[i].codpro+'">'+
+								'<div class="product">'+
+									'<img src="'+data.datos[i].rutimapro+'">'+
+									'<div class="detail-title">'+data.datos[i].nompro+'</div>'+
+									'<div class="detail-description">'+data.datos[i].despro+'</div>'+
+									'<div class="detail-price">'+formato_precio(data.datos[i].prepro)+'</div>'+
+								'</div>'+
+							'</a>'+
+						'</div>';
+					}
+					document.getElementById("space-list").innerHTML=html;
+				},
+				error:function(err){
+					console.error(err);
+				}
+			});
+		});
+		function formato_precio(valor){
+			//10.99
+			let svalor=valor.toString();
+			let array=svalor.split(".");
+			return "S/. "+array[0]+".<span>"+array[1]+"</span>";
+		}
+		function iniciar_compra(){
+			$.ajax({
+				url:'servicios/compra/validar_inicio_compra.php',
+				type:'POST',
+				data:{
+					codpro:p
+				},
+				success:function(data){
+					console.log(data);
+					if (data.state) {
+						alert(data.detail);
+					}else{
+						alert(data.detail);
+						if (data.open_login) {
+							open_login();
+						}
+					}
+				},
+				error:function(err){
+					console.error(err);
+				}
+			});
+		}
+		function open_login(){
+			window.location.href="login.php";
+		}
+	</script>
 </body>
 </html>
