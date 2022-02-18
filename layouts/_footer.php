cc796bb993e5599f65a6a65fff22cd82bd50351d
Sebastian Pérez López <shanlonp4@gmail.com>
2022-02-17 23:38:19 -0500

    Correcion de errores

--- layouts/_footer.php
+++ layouts/_footer.php
@@ -3,15 +3,15 @@
 		<div class="div-flex mb20">
 			<div class="part">
 				<h4>Quienes somos?</h4>
-				<p>Empresa dedicada al rubro de librerias y comercio electrónico</p>
+				<p>Empresa dedicada a la venta de relojes</p>
 			</div>
 			<div class="part">
 				<h4 class="mb10">Redes sociales</h4>
-				<a href="" target="_blank">Facebook: Mi tienda</a>
+				<a href="" target="_blank">Facebook: Perpet</a>
 				<br>
 				<a href="" target="_blank">Whatsapp: 999 888 777</a>
 			</div>
 		</div>
-		<center><h5>Mi tienda - Derechos reservador 2021</h5></center>
+		<center><h5>Pepert - Derechos reservador 2022</h5></center>
 	</div>
 </footer>
